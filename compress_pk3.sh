5978d3c3145673db11630805960c582d5c0f818c
Matthias Krüger <matthias.krueger@famsik.de>
2017-02-04 21:52:39 +0100

    rm -> rm -f

--- compress_pk3.sh
+++ compress_pk3.sh
@@ -9,11 +9,11 @@ echo "Converting"
 for file in `find . | grep "\.tga"`; do # in theory we could make this parallel
 	convert ${file} "${file%.tga}.png" # tga -> png
 	$FLIF -e "${file%.tga}.png" "${file%.tga}.flif" # png -> flif
-	rm $file # rm tga
-	rm ${file%.tga}.png # rm png
+	rm -f $file # rm tga
+	rm -f ${file%.tga}.png # rm png
 done
 
 echo "Compressing"
 mksquashfs * ../${1}.sqfs -comp xz -Xdict-size "100%"
 cd .. # clean up
-rm -r extractDir
+rm -fr extractDir
