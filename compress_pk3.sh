6eca29c35150ea4ac7e6613f724710b044731f05
Matthias Krüger <matthias.krueger@famsik.de>
2017-02-04 21:57:59 +0100

    compress: convert pngs to flif too (not only tga)

--- compress_pk3.sh
+++ compress_pk3.sh
@@ -13,6 +13,11 @@ for file in `find . | grep "\.tga"`; do # in theory we could make this parallel
 	rm -f ${file%.tga}.png # rm png
 done
 
+for file in `find . | grep "\.png"`; do # in theory we could make this parallel
+	$FLIF -e "${file%.png}.png" "${file%.png}.flif" # png -> flif
+	rm -f ${file%.png}.png # rm png
+done
+
 echo "Compressing"
 mksquashfs * ../${1}.sqfs -comp xz -Xdict-size "100%"
 cd .. # clean up
